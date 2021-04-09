98f13df3bcee5139fb69b8934747b88e1b17dd6c
JulienLamalle <julienlamalle63100@gmail.com>
2021-04-09 23:42:03 +0200

    pagination on properties page added

--- pages/properties.js
+++ pages/properties.js
@@ -1,28 +1,67 @@
-import React from 'react';
-import api from '../auth/axios'
-import Layout from '../components/Layout'
-import Card from '../components/Card'
-import { MDBContainer } from 'mdb-react-ui-kit';
+import React from "react";
+import Head from 'next/head'
+import api from "../auth/axios";
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+import { MDBContainer } from "mdb-react-ui-kit";
+import { useRouter } from "next/router";
+import ReactPaginate from "react-paginate";
 
-const Properties = ({ properties }) => {
+const Properties = ({ properties, currentPage, pageCount }) => {
+  const router = useRouter();
+
+  const paginationHandler = (page) => {
+    const currentPath = router.pathname;
+    const currentQuery = { ...router.query };
+    currentQuery.page = page.selected + 1;
+    router.push({
+      pathname: currentPath,
+      query: currentQuery,
+    });
+  };
   return (
-    <Layout>
-      <MDBContainer>
-        <Card properties={properties} />
-      </MDBContainer>
-    </Layout>
-  )
-}
+    <section>
+      <Head>
+        <title>WebImmo - Liste des biens</title>
+      </Head>
+      <Layout>
+        <MDBContainer>
+          <Card properties={properties} />
+          <div className="paginate-center">
+            <ReactPaginate
+              onPageChange={paginationHandler}
+              initialPage={currentPage - 1}
+              pageCount={pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              previousLabel="Precedent"
+              nextLabel="Suivant"
+              activeClassName="actived"
+              breakLabel="..."
+              pageClassName="paginate"
+              containerClassName="custom-paginate"
+            />
+          </div>
+        </MDBContainer>
+      </Layout>
+    </section>
+  );
+};
 
-export const getServerSideProps = async () => {
-  const {data} = await api.get('/api/properties')
-  const properties = data.data
-  
+export const getServerSideProps = async ({ query }) => {
+  const page = query.page || 1;
+  const { data } = await api.get(`/api/properties?page=${page}`);
+  const properties = data.data;
+  const currentPage = data.currentPage;
+  const pageCount = data.totalPages;
+
   return {
     props: {
-      properties
-    }
-  }
-}
+      properties,
+      currentPage,
+      pageCount,
+    },
+  };
+};
 
 export default Properties;
